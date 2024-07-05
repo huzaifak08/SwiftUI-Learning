b111f654de3ebe51ad7fa7b2dd2811e0c3eb01fe
huzaifak08 <hk7928042@gmail.com>
2024-07-05 18:38:30 +0500

    Stacks Allhamdullillah

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), 
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32,weight: .medium,design: .default))
+                    .foregroundColor(.white)
+//                    .background(Color.red)
+//                    .frame(width: 200,height: 200)
+                    .padding(.bottom)
+                
+                Spacer() // Expand
+                
+            }
         }
-        .padding()
     }
 }
 
