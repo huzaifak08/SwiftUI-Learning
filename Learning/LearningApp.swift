a8241ff3e976f6c6c880279383ff90552461385a
huzaifak08 <hk7928042@gmail.com>
2024-07-05 17:53:56 +0500

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Huzaifa Khan  on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
